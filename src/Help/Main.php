89e321cb44e6373a628a878fa2a1d9d0a8e1118c
iRainFlop <42165965+iRainFlop@users.noreply.github.com>
2018-08-08 17:55:13 -0700

    Update Main.php

--- src/Help/Main.php
+++ src/Help/Main.php
@@ -43,45 +43,17 @@ class Main extends PluginBase implements Listener {
 		$date = date('Y-m-d');
 		if ($sender instanceof Player) {
 			switch ($cmd->getName()) {
-				case "ctime":
-					$sender->sendMessage(Main::PREFIX . TextFormat::RED  . TextFormat::BOLD . "THE TIME IS " . $time);
-					return true;
-				case "iraindrop":
-					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "THIS PLUGIN IS MADE BY @IRAINDROP_ (THAT IS MY TWITTER)");
-					$sender->addTitle(TextFormat::RED . TextFormat::BOLD . "OOF");
-					$item = Item::get(264,0,100);
-					$inv = $sender->getInventory();
-					$item->setCustomName("Diamond");
-					$inv->addItem($item);
-					return true;
-				case "cdate":
-					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "THE DATE IS " . $date);
-					return true;
-				case "kit test":
-					$sender->addTitle(TextFormat::RED . TextFormat::BOLD . "KIT TEST");
-					$item = Item::get(264,0,100);
-					$inv = $sender->getInventory();
-					$item->setCustomName(TextFormat::RED . TextFormat::BOLD . "Diamond");
-					$item->setNamedTagEntry(new ListTag("ench"));
-					$inv->addItem($item);
-					$item = Item::get(1,0,100);
-					$inv = $sender->getInventory();
-					$item->setCustomName("Stone");
-					$inv->addItem($item);
-					return true;
-				case "ping":
-					$sender->sendMessage(TextFormat::RED . TextFormat::BOLD  .  "YOUR PING IS " . $sender->getPing() . "MS");
+				case "help":
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "- HELP -");
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "/f create");
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "/f join");
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "/f del");
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "/f about");
+					$sender->sendMessage(Main::PREFIX . TextFormat::RED . TextFormat::BOLD . "- HELP -");
 					return true;
 			}
 		} else {
 			//Code for console
-		}
-	}
-  
-	public function onJoin(PlayerJoinEvent $event){
-		$player = $event->getPlayer();
-		if($player instanceof Player) {
-		$player->getServer()->dispatchCommand($player, $this->getConfig()->get("ExecuteCommand"));
 		}
 	}
   
